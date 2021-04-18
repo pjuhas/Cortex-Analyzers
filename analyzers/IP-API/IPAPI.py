874a889dce22ccdfd0f50777d47cd42a319aa18e
ConfusedInterval <maybehero@protonmail.ch>
2021-04-18 20:15:48 +0200

    ip-api

---
+++ analyzers/IP-API/IPAPI.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import requests
+from cortexutils.analyzer import Analyzer
+
+
+class BitcoinAbuse(Analyzer):
+
+    def __init__(self):
+        Analyzer.__init__(self)
+
+    def summary(self, raw):
+        taxonomies = []
+        level = 'info'
+        namespace = 'IP-API'
+        predicate = 'Country'
+        value = "0"
+        if raw["country"] != "":
+            value = "{}".format(raw["country"])
+        taxonomies.append(self.build_taxonomy(level, namespace, predicate, value))
+        return {'taxonomies': taxonomies}
+
+    def run(self):
+        Analyzer.run(self)
+        if self.data_type == 'ip' or self.data_type == 'domain':
+            try:
+                data = self.get_data()
+                s = requests.Session()
+                response_details = s.get('http://ip-api.com/json/{}'
+                                         .format(data))
+
+                if response_details.status_code == 200 or response_details.status_code == 201:
+                    result = response_details.json()
+                    self.report(result if len(result) > 0 else {})
+                else:
+                    self.error('Failed to query IP-API details. Status_code {}'.format(response_details.status_code))
+            except Exception as e:
+                self.unexpectedError(e)
+        else:
+            self.notSupported()
+
+
+if __name__ == '__main__':
+    BitcoinAbuse().run()
